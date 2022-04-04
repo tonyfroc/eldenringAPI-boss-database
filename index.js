236bfba5038f8484e0098c7e271436a39ebab9dd
Tony Froccaro <tfroccaro@gmail.com>
2022-04-03 20:05:12 -0400

    Added functionality for switching between regions, clearing fields when switching between regions, and all bosses to their respective region available from our API

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 "use strict";
 
+// UI Logic
+
 const bossesContainer = document.querySelector(".bosses");
-// console.log(bossesContainer);
 
 const renderBoss = function (data, className) {
   if (data.healthPoints === "???") {
@@ -29,44 +30,96 @@ const renderLocation = function (data, className) {
     <p class="location_description"> <span>Description:</span> ${data.description}</p>
   </div>
 </article>`;
-bossesContainer.insertAdjacentHTML("beforeend", html);
-bossesContainer.style.opacity = 1;
+  bossesContainer.insertAdjacentHTML("beforeend", html);
+  bossesContainer.style.opacity = 1;
 };
 
 const getBossAndLocation = function (boss) {
-  // Boss
   fetch(`https://eldenring.fanapis.com/api/bosses?name=${boss}`)
-    .then(response => response.json()) // response, transformed to json
-    .then(data => {
-      renderBoss(data.data[0]); // take data, render the boss to the DOM
+    .then((response) => response.json())
+    .then((data) => {
+      renderBoss(data.data[0]);
       const bossLocation = data.data[0].location;
-
       if (!bossLocation) return;
-  // Location   
-      return fetch(`https://eldenring.fanapis.com/api/locations?name=${bossLocation}`);
+      return fetch(
+        `https://eldenring.fanapis.com/api/locations?name=${bossLocation}`
+      );
     })
-      .then(response => response.json())
-      .then(data => renderLocation(data.data[0], 'bossLocation'))
+    .then((response) => response.json())
+    .then((data) => renderLocation(data.data[0], "bossLocation"));
 };
 
+function clearFields() {
+  clearCards();
+  $("#limgrave").hide();
+  $("#weepingPeninsula").hide();
+  $("#liurniaOfTheLakes").hide();
+  $("#caelid").hide();
+  $("#altusPlateau").hide();
+  $("#mountaintopsOfTheGiants").hide();
+}
 
+function clearCards() {
+  $(".bosses").text("");
+  $(".images").text("");
+}
 
+$(document).ready(function () {
+  $(".limgrave").click(function (event) {
+    clearFields();
+    $("#limgrave").show();
+  });
+  $(".weepingPeninsula").click(function (event) {
+    clearFields();
+    $("#weepingPeninsula").show();
+  });
+  $(".liurniaOfTheLakes").click(function (event) {
+    clearFields();
+    $("#liurniaOfTheLakes").show();
+  });
+  $(".caelid").click(function (event) {
+    clearFields();
+    $("#caelid").show();
+  });
+  $(".altusPlateau").click(function (event) {
+    clearFields();
+    $("#altusPlateau").show();
+  });
+  $(".mountaintopsOfTheGiants").click(function (event) {
+    clearFields();
+    $("#mountaintopsOfTheGiants").show();
+  });
+});
 
-// Bosses with All Data and Location info
-
-getBossAndLocation('Esgar, Priest Of Blood')
-// getBossAndLocation('Elemer Of The Briar')
-// getBossAndLocation("Scaly Misbegotten");
-// getBossAndLocation("Demi-human")
-// getBossAndLocation('Ancestor Spirit')
-// getBossAndLocation('Beast Of Farum Azula')
-
-
-// Bosses with no location data
-
-// getBossAndLocation("Grafted Scion"); // Boss image no location data
-// getBossAndLocation("Flying Dragon Agheel") // Boss image no location data
-// getBossAndLocation("Leonine Misbegotten"); // Boss image no location data
-// create an object that contains the regions 
-
-// go to resources images and add the picture of the boss, but the name of the file should be the same of the id as the boss
+$(document).ready(function () {
+  $("#limgrave-bosses").on("change", function (event) {
+    clearCards();
+    const boss = $("#limgrave-bosses").val();
+    getBossAndLocation(boss);
+  });
+  $("#weepingPeninsula-bosses").on("change", function (event) {
+    clearCards();
+    const boss = $("#weepingPeninsula-bosses").val();
+    getBossAndLocation(boss);
+  });
+  $("#liurniaOfTheLakes-bosses").on("change", function (event) {
+    clearCards();
+    const boss = $("#liurniaOfTheLakes-bosses").val();
+    getBossAndLocation(boss);
+  });
+  $("#caelid-bosses").on("change", function (event) {
+    clearCards();
+    const boss = $("#caelid-bosses").val();
+    getBossAndLocation(boss);
+  });
+  $("#altusPlateau-bosses").on("change", function (event) {
+    clearCards();
+    const boss = $("#altusPlateau-bosses").val();
+    getBossAndLocation(boss);
+  });
+  $("#mountaintopsOfTheGiants-bosses").on("change", function (event) {
+    clearCards();
+    const boss = $("#mountaintopsOfTheGiants-bosses").val();
+    getBossAndLocation(boss);
+  });
+});
